cc5dc5be8c337094bc6f60747e971a4d44d9ba44
Kyouru <ren.kyouru@gmail.com>
2020-08-04 14:14:13 -0500

    Por cambio de extension

--- ENMASCARAR_DATOS.pks.sql
+++ ENMASCARAR_DATOS.pks.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PACKAGE SYS.PKG_ENMASCARAR_DATOS IS
     PROCEDURE P_ENMASCARAR_DATOS (	ENMASCARARTABLAS		IN      BOOLEAN,	--Enmascara los datos
                               		LIMPIARTABLAS			IN      BOOLEAN,	--Borra los datos de las tablas que no se encuentren en la lista
-                              		EJECUTARQUERY			IN      BOOLEAN);	--Para evitar que se ejecute los query y solo muestre por dbms
+                              		EJECUTARQUERY			IN      BOOLEAN);	--Para evitar que se ejecute los query y solo muestre por dbms = pruebas
 END PKG_ENMASCARAR_DATOS;
 /
